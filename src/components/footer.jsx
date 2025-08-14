ef8714681d5473bebffacba39c9a5578e48561f0
Triston Land <tristonland@gmail.com>
2025-08-14 00:43:16 -0600

    change footer.css to module so styles are locally scoped

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,14 @@
-import "../styles/footer.css";
+// this uses css module for importing style.
+// you have to import it as a object like "styles" or something then use it in the classname with curly braces
+import styles from "../styles/footer.module.css";
 
 function Footer() {
   return (
     <>
-      <div className="footer">
-        Made by <a href="https://tristonland.com">TristonSquad</a>
+      <div className={styles.footer}>
+        <p>
+          Made by <a href="https://tristonland.com">TristonSquad</a>
+        </p>
       </div>
     </>
   );
